fbd1c502350ecb9ced53354a598570566d8fab2a
NiaMcAlkich <104858847+NiaMcAlkich@users.noreply.github.com>
2022-10-05 14:33:33 -0400

    Correct Directory

--- DorasFileExplorer/app/src/main/java/com/example/dorasfileexplorer/MainActivity.kt
+++ DorasFileExplorer/app/src/main/java/com/example/dorasfileexplorer/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.dorasfileexplorer
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import com.example.dorasfileexplorer.ui.theme.DorasFileExplorerTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +31,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    FileExplorer()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalUnitApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FileExplorer() {
+    val storageList = listOf(Environment.getExternalStorageDirectory().absolutePath.toString())
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+    Text(
+        text = "Your Storage",
+
+        modifier = Modifier.padding(50.dp),
+
+        style = androidx.compose.ui.text.TextStyle(
+            color = Color.Black,
+            fontSize = TextUnit(value = 20.0F, type = TextUnitType.Sp)
+        ), fontWeight = FontWeight.Black
+    )
+
+    LazyColumn {
+
+        items(storageList) { storage ->
+
+            Text(storage, modifier = Modifier.padding(50.dp))
+
+            Divider()
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DorasFileExplorerTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview(){
+    FileExplorer()
 }
